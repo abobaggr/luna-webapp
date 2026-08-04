b3c1e13434173fc69dfd617b3a8dc93d413cd02a
abobaggr <132574793+abobaggr@users.noreply.github.com>
2026-08-04 07:40:28 +0300

    Add initial setup instructions to webapp.py

---
+++ webapp.py
@@ -0,0 +1,5 @@
+Quick setup — if you've done this kind of thing before
+[HTTPS] [SSH]  https://github.com/abobaggr/luna.git
+
+...or create a new repository on the command line
+...or push an existing repository from the command line
