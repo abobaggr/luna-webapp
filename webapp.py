a74cf799d78d0f2e85dbd90d9e65c6358207a26c
abobaggr <132574793+abobaggr@users.noreply.github.com>
2026-08-04 07:48:18 +0300

    Refactor webapp.py to include Flask imports
    Removed setup instructions and added necessary imports for Flask application.

--- webapp.py
+++ webapp.py
@@ -1,5 +1,7 @@
-Quick setup — if you've done this kind of thing before
-[HTTPS] [SSH]  https://github.com/abobaggr/luna.git
-
-...or create a new repository on the command line
-...or push an existing repository from the command line
+import os
+import json
+import sqlite3
+from datetime import datetime
+from flask import Flask, jsonify, request, Response
+from flask_cors import CORS
+from dotenv import load_dotenv
